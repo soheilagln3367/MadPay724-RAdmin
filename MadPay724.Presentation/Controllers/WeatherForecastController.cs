aee15f993dbdedf3a82c0990cb323c8821e31f36
soheila <s_golshan22@yahoo.com>
2020-11-10 17:26:09 +0330

    init project

---
+++ MadPay724.Presentation/Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MadPay724.Presentation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet ("{id}")]
+        public async Task<ActionResult<string>> Get(int id)
+        {
+            return "value";
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        [HttpPost]
+        public async Task<string> Post([FromBody] string vlaue)
+        {
+            return null;
+        }
+        [HttpPut ("{id}")]
+        async Task<string> Put(int id, [FromBody] string vlaue)
+        {
+            return null;
+        }
+        [HttpDelete("{id}")]
+        async Task<string> Delete(int id)
+        {
+            return null;
+        }
+    }
+}
