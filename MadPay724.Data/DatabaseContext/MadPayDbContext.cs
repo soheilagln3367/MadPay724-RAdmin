4a9a767dbd6bf8e44e21b59088d4bca8012fbc78
soheila <s_golshan22@yahoo.com>
2020-11-16 15:27:27 +0330

    04-ep0

---
+++ MadPay724.Data/DatabaseContext/MadPayDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MadPay724.Data.DatabaseContext
+{
+    //ConnectionString
+    class MadPayDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-C0S592B\SQLEXPRESS;Initial Catalog=MadPay724db;Integrated Security=True;MultipleActiveResultSets=True;");
+        }
+    }
+}
---
+++ MadPay724.Data/Models/BankCard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MadPay724.Data.Models
+{
+    public  class BankCard:BaseEntity<string>
+    {
+        public BankCard()
+        {
+            Id = Guid.NewGuid().ToString();
+            DateCreated = DateTime.Now;
+            DateModified = DateTime.Now;
+        }
+        [Required]
+        public string BankName { get; set; }
+        public string Sahba { get; set; }
+        [Required]
+        [Range(16,16)]
+        public string CardNumber { get; set; }
+        [Required]
+        [StringLength(2, MinimumLength = 2)]
+        public string ExpirDateMounth { get; set; }
+        [Required]
+        [StringLength(2, MinimumLength = 2)]
+        public string ExpirDateYear { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+        public User user { get; set; }
+    }
+}
+}
